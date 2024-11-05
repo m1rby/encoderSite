967973819203e58e302780fb1aa40ed4fe7ac5ec
mirby <seniafur2000@gmail.com>
2024-11-05 11:10:48 +0300

    version 0.9 (without tests)

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, send_file, render_template
-import subprocess
 import os
 import uuid
+from flask import Flask, request, send_file, render_template
+import subprocess
 
 app = Flask(__name__)
 
@@ -10,26 +10,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Ожидается, что index.html будет в папке "templates"
 
-# Маршрут для обработки шифрования
 @app.route('/encrypt', methods=['POST'])
 def encrypt_file():
     file = request.files['file']
     method = request.form['method']
     modulus = request.form['modulus']
     
-    # Уникальное имя файла для предотвращения коллизий
-    input_filename = f"{uuid.uuid4()}_{file.filename}"
+    # Сохраняем файл с оригинальным именем
+    input_filename = file.filename
     file.save(input_filename)
 
     # Запускаем encryptor.exe с параметрами
     subprocess.run(['./encryptor.exe', input_filename, modulus, method])
 
-    # Отправляем зашифрованный файл обратно
-    encrypted_filename = f"encrypted_{file.filename}"
-    return send_file(input_filename, as_attachment=True, download_name=encrypted_filename)
+    # Возвращаем зашифрованный файл с оригинальным именем
+    return send_file(input_filename)
 
-import os
-
+# Маршрут для обработки расшифровки
 @app.route('/decrypt', methods=['POST'])
 def decrypt_file():
     # Получаем данные от формы
@@ -38,10 +35,11 @@ def decrypt_file():
     length_byte = request.form['lengthByte']
     checksum_byte = request.form['checksumByte']
 
-    # Уникальные имена файлов для предотвращения коллизий
-    decrypt_filename = f"{uuid.uuid4()}_{file.filename}"
-    gamma_filename = f"{uuid.uuid4()}_gamma_file"
+    # Сохраняем файл с оригинальным именем
+    decrypt_filename = file.filename
     file.save(decrypt_filename)
+    
+    gamma_filename = gamma.filename
     gamma.save(gamma_filename)
 
     # Преобразуем радиокнопки в 'y' или 'n'
@@ -52,14 +50,18 @@ def decrypt_file():
     subprocess.run(['./decryptor.exe', decrypt_filename, gamma_filename, length_arg, checksum_arg], check=True)
 
 
-    # Возврат зашифрованного файла
-    return send_file(decrypt_filename, as_attachment=True, download_name=f"decrypted_{file.filename}")
+
+    # Возвращаем расшифрованный файл
+    return send_file(decrypt_filename, as_attachment=True)
+
+
 
 
 # Маршрут для скачивания файла гаммы
 @app.route('/download-gamma', methods=['GET'])
 def download_gamma():
     return send_file('gamma.txt', as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
